d45a1d86c78d801f7f62277e82f96a51da40786e
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-26 14:27:06 +0100

    - mapping via stream API

--- courseselection/src/main/java/pl/sdacademy/CourseSelectionServlet.java
+++ courseselection/src/main/java/pl/sdacademy/CourseSelectionServlet.java
@@ -7,16 +7,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "CourseSelectionServlet", value = "/courseSelection")
 public class CourseSelectionServlet extends HttpServlet {
+    private Map<String, Collection<String>> courses = new HashMap<>();
+
+    public CourseSelectionServlet() {
+        courses.put("PROGRAMMER", Arrays.asList("Java", "C#", "C++"));
+        courses.put("TESTER", Arrays.asList("Selenium", "Ranorex"));
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String courseType = request.getParameter("courseType");
+
+        courses.get(courseType);
+
         PrintWriter writer = response.getWriter();
         writer.println("<html>" +
                 "<body>" +
-                "<p>Selected course type: " + courseType + "</p>" +
+                "<p>...selected course type: " + courseType + "</p>" +
+                "<select>" +
+                courses.get(courseType).stream()
+                        .map(course -> "<option>" + course + "</option>")
+                        .collect(Collectors.joining()) +
+                "<select>" +
                 "</body>" +
                 "</html>");
     }
