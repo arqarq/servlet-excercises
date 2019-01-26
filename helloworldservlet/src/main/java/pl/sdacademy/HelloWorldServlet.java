d046a6579102b99b6f37582b7426fe83bca32797
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-26 12:37:43 +0100

    - date shown dynamically - pom.xml changed

--- helloworldservlet/src/main/java/pl/sdacademy/HelloWorldServlet.java
+++ helloworldservlet/src/main/java/pl/sdacademy/HelloWorldServlet.java
@@ -1,7 +1,26 @@
 package pl.sdacademy;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
+@WebServlet(name = "HelloWorldServlet", value = "/helloWorld")
 public class HelloWorldServlet extends HttpServlet {
-
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>" +
+                "<head>" +
+                "<title>Dynamiczna strona WWW</title>" +
+                "</head>" +
+                "<body>" +
+                "<p>Dzisiaj jest " + LocalDateTime.now() + "</p>" +
+                "</body>" +
+                "</html>");
+    }
 }
