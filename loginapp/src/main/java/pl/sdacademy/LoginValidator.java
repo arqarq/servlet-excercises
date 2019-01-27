7670e133553cf2779f5c84038b1eaa0352ee3f06
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-27 11:13:35 +0100

    - login validator TDD (only tests, RED stage)

--- loginapp/src/main/java/pl/sdacademy/LoginValidator.java
+++ loginapp/src/main/java/pl/sdacademy/LoginValidator.java
@@ -1,18 +1,18 @@
 package pl.sdacademy;
 
-public class LoginValidator {
+class LoginValidator {
     /**
      * This method validates if username and password are correct
-     * If {@code userName} or {@code password} then return false.
+     * If {@code userName} or {@code password} is empty then return false.
      * If {@code userName} or {@code password} length is smaller than 6 then return false.
      * If {@code userName} is Javaktw9 and password is SDA2019 then return true.
      * Username and password are case sensitive.
      *
-     * @param userName
-     * @param password
-     * @return
+     * @param userName given user name, case sensitive
+     * @param password given password
+     * @return returns TRUE if correct login occured or FALSE if not
      */
     boolean isValid(String userName, String password) {
-        return false;
+        return true;
     }
 }
