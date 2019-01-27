2578e65639ae00c336d44171c392e180da65049c
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-27 12:50:19 +0100

    - LoginErrorServlet started

--- loginapp/src/main/java/pl/sdacademy/LoginServlet.java
+++ loginapp/src/main/java/pl/sdacademy/LoginServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
@@ -15,7 +16,11 @@ public class LoginServlet extends HttpServlet {
         String password = request.getParameter("password");
 
         if (!new LoginValidator().isValid(user, password)) {
-            response.getWriter().println("Invalid user or password");
+//            response.getWriter().println("Invalid user or password");
+            request.setAttribute("validationMessages", Arrays.asList(
+                    "Invalid user name or password",
+                    "Password cannot be empty"));
+            request.getRequestDispatcher("/loginError").forward(request, response);
             return;
         }
         response.getWriter().println("Valid user");
