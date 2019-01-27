7684dfbc6e3d78273efa10e11c0962c331b48954
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-27 11:34:08 +0100

    - LoginServlet initial

---
+++ loginapp/src/main/java/pl/sdacademy/LoginServlet.java
@@ -0,0 +1,9 @@
+package pl.sdacademy;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+
+}
