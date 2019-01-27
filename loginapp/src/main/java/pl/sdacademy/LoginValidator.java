28b26a584c77d93b668ce4defc42344ff163d03a
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-27 10:32:27 +0100

    - login validator started

---
+++ loginapp/src/main/java/pl/sdacademy/LoginValidator.java
@@ -0,0 +1,12 @@
+package pl.sdacademy;
+
+public class LoginValidator {
+    /**
+     * @param userName
+     * @param password
+     * @return
+     */
+    boolean isValid(String userName, String password) {
+        return false;
+    }
+}
