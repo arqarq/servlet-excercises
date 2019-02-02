36463b7763a2201a37ab1a3725ad10785a816458
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-02 09:38:41 +0100

    - product names changed

--- sessionhandlingbasket/src/main/java/pl/sdacademy/articles/ArticleServices.java
+++ sessionhandlingbasket/src/main/java/pl/sdacademy/articles/ArticleServices.java
@@ -7,9 +7,9 @@ public class ArticleServices {
     public static Article noArticle = new Article(0L, "no product");
     private Collection<Article> availableArticles = Arrays.asList(
             new Article(1L, "Piwo"),
-            new Article(2L, "Piwo2"),
-            new Article(3L, "Piwo3"),
-            new Article(4L, "Piwo4"));
+            new Article(2L, "Chleb"),
+            new Article(3L, "Mleko"),
+            new Article(4L, "Sok pomarańczowy"));
 
     public Collection<Article> getAvailableArticles() {
         return availableArticles;
