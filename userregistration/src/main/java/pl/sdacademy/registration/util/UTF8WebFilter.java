e0fffb541011366328f8621a316c346da542e987
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-06 15:31:05 +0100

    - filters don't work with TomEE?

---
+++ userregistration/src/main/java/pl/sdacademy/registration/util/UTF8WebFilter.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.registration.util;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+//@WebFilter(filterName = "UTF8WebFilter", value = "/*")
+//public class UTF8WebFilter extends HttpFilter {
+//    @Override
+//    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+////        req.setCharacterEncoding("utf-8");
+////        resp.setContentType("text/html;charset=utf-8");
+//        chain.doFilter(request, response);
+//    }
+//public class UTF8WebFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//            throws IOException, ServletException {
+////        req.setCharacterEncoding("utf-8");
+////        resp.setContentType("text/html;charset=utf-8");
+//        chain.doFilter(request, response);
+//    }
+//}
