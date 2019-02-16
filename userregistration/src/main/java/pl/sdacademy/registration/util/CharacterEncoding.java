f78bf2cdfaed97e6358126894740bed3203060d5
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-17 00:18:39 +0100

    - added working filter

---
+++ userregistration/src/main/java/pl/sdacademy/registration/util/CharacterEncoding.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.registration.util;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "CharacterEncoding", value = "/*")
+public class CharacterEncoding implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        request.setCharacterEncoding("utf-8");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
