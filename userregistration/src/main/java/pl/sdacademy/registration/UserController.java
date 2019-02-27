110eb437de80b27bbc00bd74622ad095cf31d6fb
arqarq <43341071+arqarq@users.noreply.github.com>
2019-02-27 21:03:21 +0100

    - ...

--- userregistration/src/main/java/pl/sdacademy/registration/UserController.java
+++ userregistration/src/main/java/pl/sdacademy/registration/UserController.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.List;
 
 @WebServlet(name = "UserController", value = "/users")
 public class UserController extends HttpServlet {
