1fea9aff8db75483f72d61d13fca1f498f33d925
arqarq <43341071+arqarq@users.noreply.github.com>
2019-03-17 10:26:57 +0100

    - initialValue in Atomic corrected

--- jsp-hello/src/main/java/pl/sdacademy/Counter.java
+++ jsp-hello/src/main/java/pl/sdacademy/Counter.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 public enum Counter {
     INSTANCE;
 
-    private AtomicInteger count = new AtomicInteger(0);
+    private AtomicInteger count = new AtomicInteger(-1);
 
     public Integer getCount() {
         return count.incrementAndGet();
